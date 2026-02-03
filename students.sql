b6ba61ccf69ec9d67a8d69bf9e5b61e6d93d106e
Aksa <aksasaifi33@gmail.com>
2026-02-03 14:31:14 +0530

    add basic SQL scripts (students table and basics)

---
+++ students.sql
@@ -0,0 +1,13 @@
+-- Students table practice
+-- Database: SQL_Practice
+
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT
+);
+
+INSERT INTO Students VALUES (1, 'A', 22);
+INSERT INTO Students VALUES (2, 'John', 23);
+
+SELECT * FROM Students;
